9a361660315a8facef5be387fb60556370274d62
espressoneeded <laura@espressoneeded.com>
2016-11-21 14:51:21 -0500

    reset

--- app/controllers/dashing/events_controller.rb
+++ app/controllers/dashing/events_controller.rb
@@ -12,9 +12,6 @@ module Dashing
       @redis = Dashing.redis
       @redis.psubscribe("#{Dashing.config.redis_namespace}.*") do |on|
         on.pmessage do |pattern, event, data|
-          var padding = new Array(2049);
-          response.stream.write(":" + padding.join(" ") + "\n"); # 2kB padding for IE
-          response.stream.write("retry: 2000\n");
           response.stream.write("data: #{data}\n\n")
         end
       end
